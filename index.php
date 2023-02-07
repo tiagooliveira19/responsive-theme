4db19c7c0dbe5a0a556acd1b66bf8ebd4838bfb1
Tiago Oliveira <tiagojf_93@hotmail.com>
2023-02-07 14:54:14 -0300

    Menu layout

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <?php include 'metas.php'; ?>
+    <title>Responsive Theme</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="header">
+
+            <div class="title">
+                veja
+            </div>
+
+            <ul class="links">
+
+                <input type="checkbox" id="checkbox_toggle">
+                <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+                <div class="menu">
+                    <li>Diálogos vazados</li>
+                    <li>Previdência</li>
+                    <li>Radar</li>
+                    <li>Páginas Amarelas</li>
+                    <li>Revista</li>
+                    <li>Newsletters</li>
+                    <li>Podcasts</li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
